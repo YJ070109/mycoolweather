e17254711aedc74a6976982ee697b9ecc181cce0
YJ070109 <yxg_070109@163.comm>
2019-05-20 09:21:02 +0800

    再次提交

--- app/src/main/java/com/example/y/mycoolweather/WeatherActivity.java
+++ app/src/main/java/com/example/y/mycoolweather/WeatherActivity.java
@@ -133,7 +133,7 @@ public class WeatherActivity extends AppCompatActivity  {
      * 根据天气id请求城市天气信息。
      */
     public void requestWeather(final String weatherId) {
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=82ae2edcf6674059aacc7e7e80411eed";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
